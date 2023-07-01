ac9c86b0de412fc83e480e7276c3a63e70356e2f
Måns Bülling <manskron@gmail.com>
2023-07-01 22:28:36 +0200

    custom-set-variables shall pester me no more

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-(setq vc-follow-symlinks t)
 
 ;;;;;;;;;;;;;;
 ;; Packages ;;
@@ -371,16 +370,9 @@
 (setq evil-shift-width 4)
 
 (setq treesit-extra-load-path '("/User/mans/.emacs.d/tree-sitter"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(flycheck xterm-color which-key vertico tree-sitter-langs sly projectile pkg-info orderless marginalia magit lsp-mode general evil corfu consult circadian cider)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq vc-follow-symlinks t)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
