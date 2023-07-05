cec0d8ade843dc0b4b4161cf5fabf4f6cf7c3484
Måns Bülling <manskron@gmail.com>
2023-07-05 22:34:01 +0200

    cleaning

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+(setq vc-follow-symlinks t)
 
 ;;;;;;;;;;;;;;
 ;; Packages ;;
@@ -27,13 +28,18 @@
 
 (use-package projectile
   :config
-  (projectile-mode +1))
+  (projectile-mode +1)
+  :bind (
+	 ("s-p" . projectile-command-map)
+	 ("C-c p" . projectile-command-map)))
 
 (use-package magit)
 
 (use-package clojure-mode)
 
 (use-package cider)
+
+(use-package sly)
 
 (use-package lsp-mode
   :init
@@ -46,7 +52,6 @@
 
 (use-package flycheck
   :init (global-flycheck-mode))
-
 (use-package orderless
   :ensure t
   :custom
@@ -106,69 +111,6 @@
          :map minibuffer-local-map
          ("M-s" . consult-history)                 ;; orig. next-matching-history-element
          ("M-r" . consult-history))                ;; orig. previous-matching-history-element
-
-  ;; Enable automatic preview at point in the *Completions* buffer. This is
-  ;; relevant when you use the default completion UI.
-  :hook (completion-list-mode . consult-preview-at-point-mode)
-
-  ;; The :init configuration is always executed (Not lazy)
-  :init
-
-  ;; Optionally configure the register formatting. This improves the register
-  ;; preview for `consult-register', `consult-register-load',
-  ;; `consult-register-store' and the Emacs built-ins.
-  (setq register-preview-delay 0.5
-        register-preview-function #'consult-register-format)
-
-  ;; Optionally tweak the register preview window.
-  ;; This adds thin lines, sorting and hides the mode line of the window.
-  (advice-add #'register-preview :override #'consult-register-window)
-
-  ;; Use Consult to select xref locations with preview
-  (setq xref-show-xrefs-function #'consult-xref
-        xref-show-definitions-function #'consult-xref)
-
-  ;; Configure other variables and modes in the :config section,
-  ;; after lazily loading the package.
-  :config
-
-  ;; Optionally configure preview. The default value
-  ;; is 'any, such that any key triggers the preview.
-  ;; (setq consult-preview-key 'any)
-  ;; (setq consult-preview-key "M-.")
-  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
-  ;; For some commands and buffer sources it is useful to configure the
-  ;; :preview-key on a per-command basis using the `consult-customize' macro.
-  (consult-customize
-   consult-theme :preview-key '(:debounce 0.2 any)
-   consult-ripgrep consult-git-grep consult-grep
-   consult-bookmark consult-recent-file consult-xref
-   consult--source-bookmark consult--source-file-register
-   consult--source-recent-file consult--source-project-recent-file
-   ;; :preview-key "M-."
-   :preview-key '(:debounce 0.4 any))
-
-  ;; Optionally configure the narrowing key.
-  ;; Both < and C-+ work reasonably well.
-  (setq consult-narrow-key "<") ;; "C-+"
-
-  ;; Optionally make narrowing help available in the minibuffer.
-  ;; You may want to use `embark-prefix-help-command' or which-key instead.
-  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
-
-  ;; By default `consult-project-function' uses `project-root' from project.el.
-  ;; Optionally configure a different project root function.
-  ;;;; 1. project.el (the default)
-  ;; (setq consult-project-function #'consult--default-project--function)
-  ;;;; 2. vc.el (vc-root-dir)
-  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
-  ;;;; 3. locate-dominating-file
-  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
-  ;;;; 4. projectile.el (projectile-project-root)
-  ;; (autoload 'projectile-project-root "projectile")
-  ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
-  ;;;; 5. No project support
-  ;; (setq consult-project-function nil)
 )
 
 (use-package marginalia
@@ -218,11 +160,6 @@
   :init
   ;; TAB cycle if there are only few candidates
   (setq completion-cycle-threshold 3)
-
-  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
-  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
 
   ;; Enable indentation+completion using the TAB key.
   ;; `completion-at-point' is often bound to M-TAB.
@@ -332,12 +269,21 @@
 ;; Misc settings ;;
 ;;;;;;;;;;;;;;;;;;;
 
+;;Tab width
+(setq tab-width 4
+      evil-shift-width 4
+      ;;Help find tree-sitter grammars
+      treesit-extra-load-path '("/User/mans/.emacs.d/tree-sitter")
+      ;;Bind fn to super
+      ns-function-modifier 'super
+      ;;Refresh dired when files change
+      global-auto-revert-non-file-buffers t
+      ;;Start with a scratch buffer
+      inhibit-startup-message t)
+
 ;;Font size
 (set-face-attribute 'default nil
 		    :height 150)
-
-;;Start with a scratch buffer
-(setq inhibit-startup-message t)
 
 ;;Disable Menubar, Toolbars and Scrollbars
 ;; (menu-bar-mode -1)
@@ -353,8 +299,6 @@
 ;;Refresh buffer if the underlying file changes
 (global-auto-revert-mode 1)
 
-;;Refresh dired when files change
-(setq global-auto-revert-non-file-buffers t)
 
 ;;Enable recent files
 (recentf-mode 1)
@@ -365,14 +309,12 @@
 ;;Start in fullscreen
 (add-hook 'window-setup-hook 'toggle-frame-fullscreen t)
 
-;;Tab width
-(setq tab-width 4)
-(setq evil-shift-width 4)
-
-(setq treesit-extra-load-path '("/User/mans/.emacs.d/tree-sitter"))
-
-(setq vc-follow-symlinks t)
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
+
+(display-time-mode 1)
+(format-time-string "%H:%M")
+
+(global-set-key [remap eval-last-sexp] 'pp-eval-last-sexp)
